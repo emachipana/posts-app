5d39799a7c3c993014162088fa69acc37eb44412
emachipana <emachipana0102@gmail.com>
2024-10-14 13:03:57 -0500

    ADD: navbar component

---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+import { COLORS } from "../../styles/colors";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 60px;
+  padding: 1rem;
+  background-color: ${COLORS.blue};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: sticky;
+  top: 0;
+`;
+
+export const Title = styled.h1`
+  font-size: 1.2rem;
+  font-weight: 900;
+  cursor: pointer;
+`;
