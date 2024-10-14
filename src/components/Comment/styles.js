3b342fec9f1286ee5c530151c63bce7aaae56bf4
emachipana <emachipana0102@gmail.com>
2024-10-14 13:02:51 -0500

    ADD: comment component

---
+++ src/components/Comment/styles.js
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+import { COLORS } from "../../styles/colors";
+
+export const Container = styled.div`
+  border-radius: 1rem;
+  gap: 0.5rem;
+  padding: 0.5rem 0.7rem;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  background-color: ${COLORS.onyx};
+`;
