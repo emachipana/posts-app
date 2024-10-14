d9079dc0dd5ddbf09e04539996e466d32af88d2a
emachipana <emachipana0102@gmail.com>
2024-10-14 13:05:49 -0500

    ADD: posts page

---
+++ src/pages/Posts/styles.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 100%;
+  padding: 2rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+  gap: 2rem;
+`;
